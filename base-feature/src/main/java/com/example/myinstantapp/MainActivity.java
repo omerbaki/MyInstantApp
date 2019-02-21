13090377924d1ea238a29f8d4e88f728f6eac002
Omer Baki <omer@soluto.com>
2019-02-21 15:23:44 +0200

    Initial commit

---
+++ base-feature/src/main/java/com/example/myinstantapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myinstantapp;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("HERE");
+    }
+}
